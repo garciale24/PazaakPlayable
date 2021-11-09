ec24782ca9f8bcac52c014fb91646e362eff898b
Carlos Garcia <91993499+garciale24@users.noreply.github.com>
2021-11-09 14:08:27 -0800

    up

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <p>
 
-<iframe src="https://trinket.io/embed/pygame/f9650decb4?outputOnly=true" width="100%" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+<iframe src="https://trinket.io/embed/pygame/f9650decb4?outputOnly=true" width="300%" height="1000" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
 </p>
         <a
           className="App-link"
