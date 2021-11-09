9dc70a2b0f7200a7824d49931070b6847a23fa0c
Carlos Garcia <91993499+garciale24@users.noreply.github.com>
2021-11-09 14:06:49 -0800

    trinket

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
         </p>
+        <p>
+
+<iframe src="https://trinket.io/embed/pygame/f9650decb4?outputOnly=true" width="100%" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+</p>
         <a
           className="App-link"
           href="https://reactjs.org"
