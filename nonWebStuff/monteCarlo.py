daaec1ab350246dde6e3bc5f843a3f967565396b
garciale24 <c.garciale24@gmail.com>
2021-11-10 02:20:18 -0800

    webSetUp

--- nonWebStuff/monteCarlo.py
+++ nonWebStuff/monteCarlo.py
@@ -1,5 +1,12 @@
+'''
+Monte Carlo Tree Search Algorithm for Pazaak game. 
+
+The purpose of this code is to give the Pazaak game an A.I. that uses simulations to predit what the next best move is. 
+The functions in this algorithm include Selection, Expansion, Simulation, and Backpropagation.
+'''
 from my_pazaak import *
 
+# This class is used for the children of the game states in order to keep track of the move made, the state, number of wins, and number of games.
 class RootKids:
     def __init__(self, pazaakGame: PazaakState, move: int) -> None :
         self.move = move
@@ -7,6 +14,7 @@ class RootKids:
         self.wins = 0
         self.games = 0
 
+# This algorithm is the main Tree Search function containing Selection, Expansion, Simulation, and Backpropagation.
 def monte_carlo_algorithm(pazaakGame: PazaakState) -> int:
     print("monteCarloAlgo", pazaakGame.player)
     kidsList: List[RootKids] = expansion(pazaakGame)
@@ -16,11 +24,8 @@ def monte_carlo_algorithm(pazaakGame: PazaakState) -> int:
     i: int = 0
     j: int = 0
     while i < 10000:
-    #print("p2stillplaying: ", kidsList[1].state.P2stillPlaying, kidsList[1].move)
         j = 0
         for kidy in kidsList:
-            #print(kidy.state.P1setVal, kidy.state.P1stillPlaying, kidy.state.P2setVal, kidy.state.P2stillPlaying)
-            #print("player:", kidy.state.player)
             kid = simulation(kidy)
             kid = backpropagation(kid)
             kidy.state.P1setVal = pazaakGame.P1setVal
@@ -37,120 +42,72 @@ def monte_carlo_algorithm(pazaakGame: PazaakState) -> int:
     for kidy in kidsList: 
         print(kidy.games, kidy.wins)
         print(kidy.move, kidy.wins, kid.games, float(kidy.wins / kidy.games), kidy.state.P1setVal, kidy.state.P2setVal)
-    #print(kidsList[0].move, kidsList[0].wins / kidsList[0].games, kidsList[0].state.P1setVal, kidsList[0].state.P2setVal)
-    #print(kidsList[1].move, kidsList[1].wins / kidsList[1].games, kidsList[1].state.P1setVal, kidsList[1].state.P2setVal)
-
     return 0
 
+# This function performs the given A.I. move in a child state and returns the new child state. 
 def doMove(pazaakGame: PazaakState, move: int) -> PazaakState:
-    #print(move)
     childState: PazaakState = pazaakGame.makeChild()
 # -------------------------------------------#
     if move == -10:
         childState.P2setVal += -5
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
     elif move == -9: 
         childState.P2setVal += -4
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
     elif move == -8:
         childState.P2setVal += -3
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
     elif move == -7:
         childState.P2setVal += -2
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
     elif move == -6:
         childState.P2setVal += -1
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
 # -------------------------------------------#
     elif move == -5:
         childState.P2setVal += -5
-        #return childState
-
     elif move == -4:
         childState.P2setVal += -4
-        #return childState
-
     elif move == -3:
         childState.P2setVal += -3
-        #return childState
-
     elif move == -2:
         childState.P2setVal += -2
-        #return childState
-
     elif move == -1: 
         childState.P2setVal += -1
-        #return childState
-
 # -------------------------------------------#
     elif move == 1: 
         childState.P2setVal += 1
-        #return childState
-
     elif move == 2:
         childState.P2setVal += 2
-        #return childState
-
     elif move == 3:
         childState.P2setVal += 3
-        #return childState
-
     elif move == 4:
         childState.P2setVal += 4
-        #return childState
-
     elif move == 5:
         childState.P2setVal += 5
-        #return childState
-
 # -------------------------------------------#
     elif move == 6: 
         childState.P2setVal += 1
-        childState.P2stillPlaying = 0
-        #return childState
-         
+        childState.P2stillPlaying = 0         
     elif move == 7:
         childState.P2setVal += 2
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
     elif move == 8:
         childState.P2setVal += 3
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
     elif move == 9:
         childState.P2setVal += 4
-        childState.P2stillPlaying = 0
-        #return childState
-        
+        childState.P2stillPlaying = 0        
     elif move == 10:
         childState.P2setVal += 5
-        childState.P2stillPlaying = 0
-        #return childState
-        
-
+        childState.P2stillPlaying = 0        
 # -------------------------------------------#
     elif move == 11: 
         pass
-        #return childState
     elif move == 12:
         childState.P2stillPlaying = 0
-        #return childState
-    #print(childState.player)
     if childState.P2setVal >= 20:
         childState.P2stillPlaying = 0
-    
     return childState
 
 #   Moves List:
@@ -182,67 +139,23 @@ def doMove(pazaakGame: PazaakState, move: int) -> PazaakState:
 #   move:    12 -> Stand
 
 def smartStop(child: RootKids) -> int:
-    #print("in smart stop func")
-
     returnVal: int = 1
     if child.state.player == 1:
         if (child.state.P1setVal >= 18) or ((child.state.P1setVal > child.state.P2setVal) and (child.state.P2stillPlaying == 0)):
             child.state.P1stillPlaying = 0
             if child.state.P2stillPlaying == 0: returnVal = 0
-
-        '''
-        if child.state.P1setVal >= 18 or ((child.state.P2setVal < child.state.P1setVal) and (child.state.P2stillPlaying == 0)):
-            child.state.P1stillPlaying = 0
-            #print("1 ", child.state.P1setVal, child.state.P2setVal)
-            returnVal = 0
-            #print("smartStop")
-        '''
     elif child.state.player == 2:
         if (child.state.P2setVal >= 18) or ((child.state.P2setVal > child.state.P1setVal) and (child.state.P1stillPlaying == 0)):
             child.state.P2stillPlaying = 0
-            #returnVal = 0
             if child.state.P1stillPlaying == 0: returnVal = 0
-        '''
-        if child.state.P2setVal >= 18 or ((child.state.P1setVal < child.state.P2setVal) and (child.state.P1stillPlaying == 0)):
-            child.state.P2stillPlaying = 0
-            #print("2, ", child.state.P1setVal, child.state.P2setVal)
-            returnVal = 0
-        '''
-    '''
-    if child.move == 12:
-        print("player1 stats: ", child.state.P1stillPlaying, child.state.P1setVal)
-        print("player2 stats: ", child.state.P2stillPlaying, child.state.P2setVal)
-
-        print(returnVal)
-    
-        pygame.quit()
-        exit()
-    '''
-    #print(child.state.P1setVal, child.state.P2setVal, "move:", child.move)
-
-
-
-    #time.sleep(5)
     return returnVal
 
 # function to expand the root node and return a list of kids
 def expansion(pazaakGame: PazaakState) -> List[RootKids]:
-    #print("expand")
     adder: int = 0
     childs: List[RootKids] = []
-
-    #pstate = doMove(pazaakGame, 12)
-    #print(pstate.P2stillPlaying)
-
-    #pygame.quit()
-    #exit()
-
-
     childs.append(RootKids(doMove(pazaakGame, 11), 11))
     childs.append(RootKids(doMove(pazaakGame, 12), 12))
-    #print(childs[0].state.P2stillPlaying)
-
-    #print(childs[1].state.P2stillPlaying)
     for card in pazaakGame.P2sideCards:
         if pazaakGame.P2setVal >= 13:
             if card[0] < 0:
@@ -259,77 +172,36 @@ def expansion(pazaakGame: PazaakState) -> List[RootKids]:
             if pazaakGame.P2setVal > 20:
                 childs.append(RootKids(doMove(pazaakGame, card[1]), card[1]))
                 childs.append(RootKids(doMove(pazaakGame, card[1]-5), card[1]-5))
-        #print(card)
-    #for kid in childs:
-    #    print(kid.state.P2setVal)
     return childs
 
 # function to simulate children and return 
 def simulation(child: RootKids) -> RootKids:
-    #print("simulate")
     nextCard: int = 0
     while child.state.P1stillPlaying == 1 or child.state.P2stillPlaying == 1:
-
         if (child.state.P2setVal > 20) or (child.state.P1setVal > 20):
-            #print(child.state.P1setVal, child.state.P2setVal, "move:", child.move)
-
             temp = child.state.whoWon()
-
-            #print("p1 still playing", child.state.P1stillPlaying, "setVal", child.state.P1setVal)
-            #print("p2 still playing", child.state.P2stillPlaying, "setVal", child.state.P2setVal)
-            #print("broken2!", temp)
             break
-
         if child.state.player == 1 and child.state.P2stillPlaying == 1:
             child.state.player = 2
         elif child.state.player == 2 and child.state.P1stillPlaying == 1:
             child.state.player = 1
-        
-
-
         nextCard = child.state.nextCard()
-        #print(child.state.P1stillPlaying, child.state.P2stillPlaying)
-        #pygame.quit()
-        #exit()
         if child.state.player == 1 and child.state.P1stillPlaying == 1:
             child.state.P1setVal += nextCard
         elif child.state.player == 2 and child.state.P2stillPlaying == 1:
-            #print("what the f")
             child.state.P2setVal += nextCard
-
-
         if smartStop(child) == 0:
             temp = child.state.whoWon()
-
-            #if temp == 2:
-            #    print(child.state.P1setVal, child.state.P2setVal)
-            #print("p1 still playing", child.state.P1stillPlaying, "setVal", child.state.P1setVal)
-            #print("p2 still playing", child.state.P2stillPlaying, "setVal", child.state.P2setVal)
-            #print("broken!", temp)
             break
-        
-
-
-
-        #print(child.state.player)
-
-        #print(nextCard, child.state.P1setVal, child.state.P2setVal)
-
-
-
-    #print(child.state.whoWon())
     return child
 
 def backpropagation(child: RootKids) -> RootKids:
-    #print("backpropagate")
     winner: int = 0
     winner = child.state.whoWon()
     if winner == 2:
         child.wins += 1
     child.games += 1
     return child
-
-
 
 def main():
     pGame: PazaakState = PazaakState(2)
