d939250bb59e543020ea06f5ba14634c92e8111b
garciale24 <c.garciale24@gmail.com>
2022-03-18 13:50:49 -0700

    evenMoreFinished99

--- src/components/AdAI.jsx
+++ src/components/AdAI.jsx
@@ -18,10 +18,10 @@ function AdAI() {
         </div>
       </div>
 
-    <iframe title="Tab1" width="1000" height="1000px" src="https://public.tableau.com/views/Book2_16466462420050/Sheet1?:embed=y&:display_count=yes&:showVizHome=no">
+    <iframe scrolling="no" title="Tab1" width="1200" height="1000px" src="https://public.tableau.com/views/Book2_16466462420050/Sheet1?:embed=y&:display_count=yes&:showVizHome=no">
     </iframe>
     <br></br>
-    <iframe title="Tab2" width="1000" height="500px" src="https://public.tableau.com/views/Book2_1_16466464238500/Sheet2?:embed=y&:display_count=yes&:showVizHome=no">
+    <iframe scrolling="no" title="Tab2" width="1200" height="500px" src="https://public.tableau.com/views/Book2_1_16466464238500/Sheet2?:embed=y&:display_count=yes&:showVizHome=no">
     </iframe>
     </div>
   );
