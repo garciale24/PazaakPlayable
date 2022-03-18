0a827ba0e37c0b35c2b22c62c8f21148c243e8d5
garciale24 <c.garciale24@gmail.com>
2022-03-18 15:57:48 -0700

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div className="home">
       <div class="container">
 
-        <iframe title= "docs"allowpaymentrequest="" frameborder="0" height="12900px" name="donorbox" scrolling="no"seamless="seamless" 
+        <iframe title= "docs"allowpaymentrequest="" frameborder="0" height="13000px" name="donorbox" scrolling="no"seamless="seamless" 
         src="https://docs.google.com/document/d/e/2PACX-1vSQNqtJfC7sw5c_G4A7Epmm3eLIicW8TNyPuCJSUuZQBxa7qWsoY66zIKTq7LCmZi__7S9R5z_pCMfQ/pub?embedded=true"width="850"></iframe>
 
       </div>
