c0291f529d5c99d6f909d9b410f0daae0dc1f890
garciale24 <c.garciale24@gmail.com>
2022-03-18 13:40:12 -0700

    finished99

--- src/components/HowToPlay.jsx
+++ src/components/HowToPlay.jsx
@@ -12,8 +12,10 @@ function HowToPlay() {
             {/*<h1 class="font-weight-light">How To Play</h1>*/}
             
           </div>
-          </div>      <iframe title="how to play pazaak" src="https://docs.google.com/document/d/e/2PACX-1vRKV3acIaeO-nrlKP8sONu5RXFPfCkkDiY8EwVf05YhFMGuvNEvj44EOJ5OWRoSJdzEDWtR2ox4MfPc/pub?embedded=true" height="2200" width= "850" seamless="seamless" frameborder="0" 
+          </div>      <iframe title="how to play pazaak" src="https://docs.google.com/document/d/e/2PACX-1vRKV3acIaeO-nrlKP8sONu5RXFPfCkkDiY8EwVf05YhFMGuvNEvj44EOJ5OWRoSJdzEDWtR2ox4MfPc/pub?embedded=true" height="1850" width= "850" seamless="seamless" frameborder="0" 
       ></iframe>
+      <iframe width="650" height="415" src="https://www.youtube.com/embed/9m3dxk8wWSo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br><br></br>
       </div>
     </div>
   );
